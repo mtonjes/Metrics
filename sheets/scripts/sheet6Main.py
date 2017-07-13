3bdd80230809c95cbd2783f14bbec40acdc4c86a
Marguerite <mtonjes@gmail.com>
2017-07-13 15:23:02 -0500

    gender added to code; fixed unicode error; fixed typo in generate

--- sheets/scripts/sheet6Main.py
+++ sheets/scripts/sheet6Main.py
@@ -156,7 +156,7 @@ def CheckDate(entry, format):
 
 def load_usa_lpc_members_csv():
     usa_lpc_members = {}
-    LPC = Read('data/LPCauthors-08April2014_fromLPCsurvey_mod.csv').split('\n')
+    LPC = Read('data/LPCauthors-06June2017_fromLPCsurvey_GV_DR.csv').split('\n')
     for line in LPC[1:]:
         columns = line.split("|")
         if len(columns) > 6:
