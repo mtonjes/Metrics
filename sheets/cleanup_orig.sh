00cd911e2a794b5eb68cd33181bfb589da530d29
Marguerite <mtonjes@gmail.com>
2024-08-13 13:53:12 -0500

    cern_sso.py
    May 2023 update files from the sheets directory, CERN SSO without 2FA

---
+++ sheets/cleanup_orig.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+rm -Rf sheets 
+cd data
+rm -rf analyses/ authors/ detail_pages/
+rm -f analyses.html annotes.html sheet1.json sheet2.json
---
+++ sheets/generate_pre.sh
@@ -0,0 +1,7 @@
+##!/bin/bash
+echo "PreRequisites"
+python scripts/prerequisites.py
+
+### echo "Generating Sheet1"
+### python scripts/sheet1Main.py
+
